f0f0b34a95deb4dbb2f3b9757c6ea68c4117830a
Julie <39500350+JGD625@users.noreply.github.com>
2019-06-14 18:28:01 -0400

    Update NoteListMain.js

--- src/NoteListMain/NoteListMain.js
+++ src/NoteListMain/NoteListMain.js
@@ -1,40 +1,49 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { Component } from 'react'
 import Note from '../Note/Note'
-import CircleButton from '../CircleButton/CircleButton'
+import NoteContext from '../NoteContext'
+import { getNotesForFolder } from '../notes-helpers'
+import LinkButton from '../LinkButton/LinkButton.js'
+import LoadingError from '../LoadingError'
 import './NoteListMain.css'
 
-export default function NoteListMain(props) {
-  return (
-    <section className='NoteListMain'>
-      <ul>
-        {props.notes.map(note =>
-          <li key={note.id}>
-            <Note
-              id={note.id}
-              name={note.name}
-              modified={note.modified}
-            />
-          </li>
-        )}
-      </ul>
-      <div className='NoteListMain__button-container'>
-        <CircleButton
-          tag={Link}
-          to='/add-note'
-          type='button'
-          className='NoteListMain__add-note-button'
-        >
-          <FontAwesomeIcon icon='plus' />
-          <br />
-          Note
-        </CircleButton>
-      </div>
-    </section>
-  )
+class NoteListMain extends Component {
+  static defaultProps = {
+    match: {
+        params: {}
+    }
+  }
+  static contextType = NoteContext;
+  render(){
+    const { notes } = this.context
+    const { folderId } = this.props.match.params
+    const notesForFolder = getNotesForFolder(notes, parseInt(folderId))
+
+    return (
+      <section className='NoteListMain'>
+        <ul>
+          {notesForFolder.map(note =>
+            <li key={note.id}>
+              <LoadingError key={note.id}>
+                <Note
+                  id={note.id}
+                  name={note.note_name}
+                  modified={note.modified_date}
+                />
+              </LoadingError>
+            </li>
+          )}
+        </ul>
+        <div className='NoteListMain__button-container'>
+          <LinkButton
+            to='/add-note'
+            className='NoteListMain__add-note-button'
+          >
+            Add Note!
+          </LinkButton>
+        </div>
+      </section>
+    )
+  }
 }
 
-NoteListMain.defaultProps = {
-  notes: [],
-}
+export default NoteListMain;
