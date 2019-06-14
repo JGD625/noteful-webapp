3d0dee90eee6dff07387140cf038a7ff41a02270
Julie <39500350+JGD625@users.noreply.github.com>
2019-06-14 18:32:11 -0400

    Update NotePageNav.js

--- src/NotePageNav/NotePageNav.js
+++ src/NotePageNav/NotePageNav.js
@@ -1,32 +1,41 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import CircleButton from '../CircleButton/CircleButton'
+import React, { Component } from 'react'
+import NoteContext from '../NoteContext'
+import { findNote, findFolder } from '../notes-helpers'
 import './NotePageNav.css'
 
-export default function NotePageNav(props) {
-  return (
-    <div className='NotePageNav'>
-      <CircleButton
-        tag='button'
-        role='link'
-        onClick={() => props.history.goBack()}
-        className='NotePageNav__back-button'
-      >
-        <FontAwesomeIcon icon='chevron-left' />
-        <br />
-        Back
-      </CircleButton>
-      {props.folder && (
-        <h3 className='NotePageNav__folder-name'>
-          {props.folder.name}
-        </h3>
-      )}
-    </div>
-  )
-}
-
-NotePageNav.defaultProps = {
-  history: {
-    goBack: () => {}
-  }
+export default class NotePageNav extends Component{
+    static defaultProps = {
+        history: {
+            goBack: () => {}
+        },
+        match: {
+            params: {}
+        }
+    }
+    static contextType = NoteContext;
+    
+    render(){
+        const { folders, notes } = this.context
+        const { noteId } = this.props.match.params
+        const note = findNote(notes, noteId) || {}
+        const folder = findFolder(folders, note.folderId)
+        return(
+            <div className='notePageNav'>
+                <button
+                    tag='button'
+                    role='link'
+                    onClick={() => this.props.history.goBack()}
+                    className='NotePageNav__back-button'
+                >
+                <br/>
+                Back
+                </button>
+                {folder && (
+                    <h3 className='NotePageNav__folder-name'>
+                        {folder.folder_name}
+                    </h3>
+                )}
+            </div>
+        )
+    }
 }
