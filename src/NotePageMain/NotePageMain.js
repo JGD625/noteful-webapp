dbe1b0163b344462791e0cd20e6ad63e1aad1301
Julie <39500350+JGD625@users.noreply.github.com>
2019-06-14 18:32:48 -0400

    Update NotePageMain.js

--- src/NotePageMain/NotePageMain.js
+++ src/NotePageMain/NotePageMain.js
@@ -1,26 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Note from '../Note/Note'
+import NoteContext from '../NoteContext'
+import { findNote } from '../notes-helpers'
 import './NotePageMain.css'
 
-export default function NotePageMain(props) {
-  return (
-    <section className='NotePageMain'>
-      <Note
-        id={props.note.id}
-        name={props.note.name}
-        modified={props.note.modified}
-      />
-      <div className='NotePageMain__content'>
-        {props.note.content.split(/\n \r|\n/).map((para, i) =>
-          <p key={i}>{para}</p>
-        )}
-      </div>
-    </section>
-  )
-}
-
-NotePageMain.defaultProps = {
-  note: {
-    content: '',
+export default class NotePageMain extends Component {
+  static defaultProps = {
+    match: {
+        params: {}
+    }
+  }
+  static contextType = NoteContext;
+  render(){
+    const { notes } = this.context
+    const { noteId } = this.props.match.params
+    const note = findNote(notes, parseInt(noteId)) || { content: '' }
+    return (
+      <section className='NotePageMain'>
+        <Note
+          id={note.id}
+          name={note.note_name}
+          modified={note.modified_date}
+          onDeleteNote={this.handleDeleteNote}
+        />
+        <div className='NotePageMain__content'>
+          {note.content.split(/\n \r|\n/).map((para, i) =>
+            <p key={i}>{para}</p>
+          )}
+        </div>
+      </section>
+    )
   }
 }
