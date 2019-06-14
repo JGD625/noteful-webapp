1b6ddeb7e8d745a2a73e68b38d5591245c63a030
Julie <39500350+JGD625@users.noreply.github.com>
2019-06-14 18:25:21 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 
 export default {
-    API_ENDPOINT: 'https://frozen-escarpment-37250.herokuapp.com'
+    API_ENDPOINT: 'https://frozen-escarpment-37250.herokuapp.com',
     //API_ENDPOINT: 'http://localhost:8000'
+     API_KEY: process.env.REACT_APP_API_KEY
   }
