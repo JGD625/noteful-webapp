64ab802f8b0400996dd3e3206e6058f2e3a3994d
Julie <39500350+JGD625@users.noreply.github.com>
2019-06-14 18:26:45 -0400

    Update Note.js

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -1,31 +1,73 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { format } from 'date-fns'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom'
 import './Note.css'
+import { format } from 'date-fns'
+import NoteContext from '../NoteContext.js'
+import PropTypes from 'prop-types'
 
-export default function Note(props) {
-  return (
-    <div className='Note'>
-      <h2 className='Note__title'>
-        <Link to={`/note/${props.id}`}>
-          {props.name}
-        </Link>
-      </h2>
-      <button className='Note__delete' type='button'>
-        <FontAwesomeIcon icon='trash-alt' />
-        {' '}
-        remove
-      </button>
-      <div className='Note__dates'>
-        <div className='Note__dates-modified'>
-          Modified
-          {' '}
-          <span className='Date'>
-            {format(props.modified, 'Do MMM YYYY')}
-          </span>
-        </div>
-      </div>
-    </div>
-  )
+class Note extends Component{
+    static defaultProps = {
+        history: {
+          push: () => { },
+          goBack: () => {}
+        },
+    }
+    static contextType = NoteContext;
+
+    handleClickDelete = e => {
+        e.preventDefault()
+        const noteId = this.props.id
+        console.log('id', noteId)
+        fetch(`https://frozen-escarpment-37250.herokuapp.com/api/notes/${noteId}`, {
+            method: 'DELETE',
+            headers: {
+              'content-type': 'application/json'
+            },
+          })
+            .then(res => {
+              if (!res.ok)
+                return res.json().then(e => Promise.reject(e))
+              return res.json()
+            })
+            .then(resJson => {
+              console.log('id2', resJson.noteId)
+              this.context.deleteNote(resJson.noteId)
+              this.props.history.push(`/`)
+            })
+            .catch(error => {
+              console.log('delete note ',{ error })
+            })
+ }
+    render(){
+        const { name, id, modified } = this.props
+        return(
+          <form onSubmit={this.handleClickDelete}>
+          <div className='Note'>
+              <h2 className='Note__title'>
+                  <Link to={`/note/${id}`}>
+                      {name}
+                  </Link>
+              </h2>
+              <button 
+                  className='Note__delete'
+                  type='submit'>Delete</button>
+              <div className='Note__dates'>
+                  <div className='Note__dates-modified'>
+                      Modified
+                      {' '}
+                      <span className='Date'>
+                          {format(modified, 'Do MMM YYYY')}
+                      </span>
+                  </div>
+              </div>
+          </div>
+          </form>
+        )
+    }
 }
+Note.propTypes = {
+  modified: PropTypes.string,
+  name: PropTypes.string,
+  id: PropTypes.number,
+};
+export default withRouter(Note)
