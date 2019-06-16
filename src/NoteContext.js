ca2c455856a66710373521975cb8d73ff7c57c51
Julie <39500350+JGD625@users.noreply.github.com>
2019-06-16 12:34:21 -0400

    Update and rename Context.js to NoteContext.js

---
+++ src/NoteContext.js
@@ -0,0 +1,13 @@
+    
+import React from 'react'
+
+const NoteContext = React.createContext({
+    deleteNote: () => {},
+    addNote: () => {},
+    addFolder: () => {},
+    folders: [],
+    notes:[],
+
+})
+
+export default NoteContext
